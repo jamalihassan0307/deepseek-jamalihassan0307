22150529b4536eafdb9fe759fa1808c2ed13faee
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-02-14 04:59:46 +0500

    button update

--- src/App.tsx
+++ src/App.tsx
@@ -209,11 +209,11 @@ function App() {
               </div>
               <button
                 type="submit"
-                className="send-button"
+                className="send-button visible"
                 disabled={!input.trim() || isLoading}
               >
                 <svg viewBox="0 0 24 24" className="send-icon">
-                  <path d="M22 2L11 13M22 2L15 22L11 13M11 13L2 9" />
+                  <path d="M2 2l20 10-20 10 3-10 17-2-17-2 3-10z" />
                 </svg>
               </button>
             </div>
